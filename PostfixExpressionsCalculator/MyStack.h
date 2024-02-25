ee57e949610f6be3d55defd751b170b0d729563c
Abdallah <abdallahmahfouz111@gmail.com>
2024-02-25 07:54:32 +0200

    update Class MyStack

--- PostfixExpressionsCalculator/MyStack.h
+++ PostfixExpressionsCalculator/MyStack.h
@@ -13,17 +13,17 @@ public:
 
 		initilization(sizeOfstack);
 	}
-	 void  initilization(int sizeOfstack)override
+	void  initilization(int sizeOfstack)override
 	{
 		this->size = sizeOfstack;
 		items = new T[size];
 		topOfstack = 0;
 	}
-	 bool IsEmpty()override
+	bool IsEmpty()override
 	{
 		return this->topOfstack == 0;
 	}
-	 bool IsFull()override {
+	bool IsFull()override {
 		return this->topOfstack == this->size;
 	}
 
@@ -34,7 +34,8 @@ public:
 			this->items[topOfstack] = item;
 			this->topOfstack += 1;
 		}
-		throw runtime_error("Stack is Full");
+		else
+			throw runtime_error("Stack is Full");
 	}
 	T Top()override {
 		if (!IsEmpty())
@@ -42,7 +43,8 @@ public:
 			T topItem = this->items[topOfstack - 1];
 			return topItem;
 		}
-		throw runtime_error("Stack is empty");
+		else
+			throw runtime_error("Stack is empty");
 	}
 	void Pop()override
 	{
@@ -50,16 +52,17 @@ public:
 		{
 			this->topOfstack -= 1;
 			T topItem = this->items[topOfstack];
-		}	
-		throw runtime_error("Stack is empty");
+		}
+		else
+			throw runtime_error("Stack is empty");
 	}
 	void CopyStack(MyStack<T>& otherStack)
 	{
 		delete[] items;
 		size = otherStack.size;
 		topOfstack = otherStack.topOfstack;
-			items = new T[size];
-		
+		items = new T[size];
+
 		for (int j = 0; j < topOfstack; j++)
 			items[j] = otherStack.items[j];
 	}
--- PostfixExpressionsCalculator/PostfixExpressionsCalculator.cpp
+++ PostfixExpressionsCalculator/PostfixExpressionsCalculator.cpp
@@ -6,6 +6,14 @@ int main()
 {
 	MyStack<int> t(10);
 	t.Push(1);
+	t.Push(2);
+	t.Push(3);
 	t.Push(4);
-	cout << t.Top() << endl;
+	t.Push(5);
+	t.Push(6);
+	while (!t.IsEmpty())
+	{
+		cout << t.Top() << " ";
+		t.Pop();
+	}
 }
